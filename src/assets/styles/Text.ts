d5c7f96399da4b1df052c7d6dd82125a807c406f
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-02 16:03:26 -0300

    feat: create base text

---
+++ src/assets/styles/Text.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native'
+import colors from '../colors'
+
+const Text = styled.Text`
+	font-family: 'System';
+	font-style: italic;
+	color: ${colors.FONT_DARK};
+`
+
+export default Text
