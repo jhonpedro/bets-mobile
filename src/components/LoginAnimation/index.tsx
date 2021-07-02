3ca3af18c1611fc37c5199c29ee328b6c9e1c2eb
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-02 16:03:58 -0300

    feat: create login animation

---
+++ src/components/LoginAnimation/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react'
+import { Animated } from 'react-native'
+
+import getDimensions from '../../utils/getDimensions'
+import {
+	LoginAnimationBox,
+	LoginAnimationText,
+	LoginAnimationTextSmallGreen,
+} from './styles'
+
+const LoginAnimation = () => {
+	const goUp = useRef(new Animated.Value(getDimensions().height * 2)).current
+
+	useEffect(() => {
+		Animated.timing(goUp, {
+			toValue: getDimensions(1).height,
+			duration: 2000,
+			useNativeDriver: true,
+		}).start(() => {
+			setTimeout(() => {
+				Animated.timing(goUp, {
+					toValue: getDimensions().height * -1.5,
+					duration: 1500,
+					useNativeDriver: true,
+				}).start()
+			}, 2000)
+		})
+	}, [goUp])
+
+	return (
+		<Animated.View
+			style={{
+				flex: 1,
+				position: 'absolute',
+				width: getDimensions().width,
+				transform: [{ translateY: goUp }],
+				zIndex: 2,
+			}}
+		>
+			<LoginAnimationBox>
+				<LoginAnimationText>The</LoginAnimationText>
+				<LoginAnimationText>Greatest</LoginAnimationText>
+				<LoginAnimationText>App</LoginAnimationText>
+				<LoginAnimationTextSmallGreen>for</LoginAnimationTextSmallGreen>
+				<LoginAnimationText size={3.5}>LOTTERY</LoginAnimationText>
+			</LoginAnimationBox>
+		</Animated.View>
+	)
+}
+
+export default LoginAnimation
---
+++ src/components/LoginAnimation/styles.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components/native'
+import colors from '../../assets/colors'
+import Text from '../../assets/styles/Text'
+import getDimensions from '../../utils/getDimensions'
+
+export const LoginAnimationBox = styled.View`
+	flex: 1;
+	justify-content: center;
+	align-items: center;
+	height: ${`${getDimensions(110).height}px`};
+	background-color: ${colors.BG_LIGHT};
+	border-radius: ${getDimensions(2).rem};
+`
+
+interface LoginAnimationTextProps {
+	size?: number
+}
+
+export const LoginAnimationText = styled(Text)<LoginAnimationTextProps>`
+	font-size: ${(props) => getDimensions(props.size ?? 2.5).rem};
+	font-weight: bold;
+`
+
+export const LoginAnimationTextSmallGreen = styled(Text)`
+	background-color: ${colors.TGL_GREEN};
+	color: #fff;
+	font-size: ${getDimensions(1).rem};
+	margin: ${getDimensions(1.3).rem} 0 0;
+	padding: ${getDimensions(0.1).rem} ${getDimensions(1).rem};
+	border-radius: ${getDimensions(1).rem};
+`
