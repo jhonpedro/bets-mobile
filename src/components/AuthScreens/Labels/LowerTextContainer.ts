9b471b7f36b81cfef6e575ededa8fee14a478b60
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-05 15:09:40 -0300

    feat: create a lower text container

---
+++ src/components/AuthScreens/Labels/LowerTextContainer.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+import getDimensions from '../../../utils/getDimensions'
+
+const LowerTextContainer = styled.TouchableOpacity`
+	margin-top: ${getDimensions(1.2).rem};
+	margin-bottom: ${getDimensions(0.1).rem};
+`
+
+export default LowerTextContainer
