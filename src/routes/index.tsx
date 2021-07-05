4a713e860b86f9d2c251f93ac792a289b969b9b4
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-05 15:12:01 -0300

    feat: apply new routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import Login from '../pages/Login'
-import colors from '../assets/colors'
 
-const AuthStack = createStackNavigator()
+import Login from '../pages/Login'
+import ForgotPassword from '../pages/ForgotPassword'
+import SingUp from '../pages/SingUp'
+import colors from '../assets/colors'
+import { AuthStackList } from '../@types'
+
+const AuthStack = createStackNavigator<AuthStackList>()
 
 const Routes: React.FC = () => (
 	<NavigationContainer>
@@ -17,6 +21,8 @@ const Routes: React.FC = () => (
 			}}
 		>
 			<AuthStack.Screen name="Login" component={Login} />
+			<AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+			<AuthStack.Screen name="SingUp" component={SingUp} />
 		</AuthStack.Navigator>
 	</NavigationContainer>
 )
