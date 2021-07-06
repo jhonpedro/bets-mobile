c35bcc8920f439730d9cf4e70dcce2eae0f67e03
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-06 16:34:35 -0300

    feat: add store provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { Provider } from 'react-redux'
 
 import Routes from './src/routes'
 import { LoadingProvider } from './src/hooks/useLoading'
 import Loading from './src/components/UI/Loading'
+import store from './src/store'
 
 export default function App() {
 	return (
-		<LoadingProvider>
-			<Routes />
-			<Loading />
-			<StatusBar style="auto" />
-		</LoadingProvider>
+		<Provider store={store}>
+			<LoadingProvider>
+				<Routes />
+				<Loading />
+				<StatusBar style="auto" />
+			</LoadingProvider>
+		</Provider>
 	)
 }
