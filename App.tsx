fbcc1d4113d3da908a6ed6c90a4cafe5d415179b
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-06 12:10:45 -0300

    feat: add loading provider and component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+
 import Routes from './src/routes'
+import { LoadingProvider } from './src/hooks/useLoading'
+import Loading from './src/components/UI/Loading'
 
 export default function App() {
 	return (
-		<>
+		<LoadingProvider>
 			<Routes />
+			<Loading />
 			<StatusBar style="auto" />
-		</>
+		</LoadingProvider>
 	)
 }
