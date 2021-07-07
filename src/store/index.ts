efaa2a272357a51de49dc0dee01ead6ec3f71cf3
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-07 16:13:52 -0300

    feat: add redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,24 @@
 import { applyMiddleware, createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
 import createSagaMiddleware from 'redux-saga'
 import rootReducer from './reducers/rootReducer'
 import rootSaga from './reducers/rootSaga'
 
+const persistedReducer = persistReducer(
+	{ key: 'tgl', storage: AsyncStorage },
+	rootReducer
+)
 const sagaMiddleware = createSagaMiddleware()
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware))
 
 sagaMiddleware.run(rootSaga)
+
+const persistor = persistStore(store)
+
+export { persistor }
 
 export default store
 
