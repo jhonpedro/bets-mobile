5afe1abeaab9d4b92fd994711ac3c0e4243a7504
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-08 11:57:09 -0300

    style: create game and games interface and

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -7,3 +7,16 @@ export type AuthStackList = {
 export type AppTabList = {
 	Home: undefined
 }
+
+export type GameI = {
+	id: number
+	type: string
+	description: string
+	range: number
+	price: number
+	max_number: number
+	color: string
+	min_cart_value: number
+}
+
+export type GamesI = Array<GameI>
