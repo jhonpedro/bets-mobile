11d22af2d14b4da8dbad0afe998f7b4460afeae4
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-12 12:08:07 -0300

    feat: add base color to navigator container

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -8,10 +8,16 @@ import Routes from './src/routes'
 import { LoadingProvider } from './src/hooks/useLoading'
 import Loading from './src/components/UI/Loading'
 import store, { persistor } from './src/store'
+import colors from './src/assets/colors'
 
 export default function App() {
 	return (
-		<NavigationContainer>
+		<NavigationContainer
+			theme={{
+				...DefaultTheme,
+				colors: { ...DefaultTheme.colors, background: colors.BG_LIGHT },
+			}}
+		>
 			<LoadingProvider>
 				<Provider store={store}>
 					<PersistGate loading={<Loading instantShow />} persistor={persistor}>
