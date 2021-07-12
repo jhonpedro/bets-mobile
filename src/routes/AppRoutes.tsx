b556d390268308514e5f98d495409a67e759f049
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-12 12:07:53 -0300

    feat: add new routes purchase and account

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
 import Home from '../pages/Home'
+import TabBar from '../components/TabBar'
+import NewPurchase from '../pages/NewPurchase'
 
 const AppTab = createBottomTabNavigator()
 
 const AppRoutes = () => (
-	<AppTab.Navigator>
+	<AppTab.Navigator tabBar={(props) => <TabBar {...props} />}>
 		<AppTab.Screen name="Home" component={Home} />
+		<AppTab.Screen name="Bet" component={NewPurchase} />
+		<AppTab.Screen name="Account" component={Home} />
 	</AppTab.Navigator>
 )
 
