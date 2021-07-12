27b330ca5eebeacf3e8386ede9266e2d2a3c4646
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-12 15:41:52 -0300

    fix: decreate buttons height

--- src/components/Game/ButtonList/styles.ts
+++ src/components/Game/ButtonList/styles.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components/native'
 import getDimensions from '../../../utils/getDimensions'
 
 export const GameButtonListContainer = styled.ScrollView`
-	height: ${getDimensions(3).rem};
+	height: ${getDimensions(2.2).rem};
 	margin-top: 5px;
 `
