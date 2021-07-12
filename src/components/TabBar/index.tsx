69798397d53bd1acf3788be113df14349e1c2b77
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-12 11:53:54 -0300

    feat: adding box shadow

--- src/components/TabBar/index.tsx
+++ src/components/TabBar/index.tsx
@@ -9,6 +9,7 @@ import {
 	BetIcon,
 	BetIconContainer,
 	TabBarContainer,
+	TabBarWrapper,
 	TabItemsWrapper,
 } from './styles'
 import { PossibleIcons as PossibleIconsType } from '../../@types'
@@ -24,7 +25,7 @@ const style = StyleSheet.create({
 	boxShadow: {
 		shadowColor: '#000',
 		shadowOpacity: 0.8,
-		shadowOffset: { width: 0, height: 2 },
+		shadowOffset: { width: 0, height: 10 },
 		shadowRadius: 2,
 		elevation: 20,
 	},
@@ -34,36 +35,38 @@ const TabBar = ({
 	state,
 	navigation,
 }: BottomTabBarProps<BottomTabBarOptions>): JSX.Element => (
-	<TabBarContainer>
-		{state.routes.map((route, index) => {
-			const isFocused = state.index === index
-			const icon = PossibleIcons[route.name]
+	<TabBarWrapper>
+		<TabBarContainer style={style.boxShadow}>
+			{state.routes.map((route, index) => {
+				const isFocused = state.index === index
+				const icon = PossibleIcons[route.name]
 
-			const onPress = () => {
-				const event = navigation.emit({
-					type: 'tabPress',
-					target: route.key,
-					canPreventDefault: true,
-				})
+				const onPress = () => {
+					const event = navigation.emit({
+						type: 'tabPress',
+						target: route.key,
+						canPreventDefault: true,
+					})
 
-				if (!isFocused && !event.defaultPrevented) {
-					navigation.navigate(route.name)
+					if (!isFocused && !event.defaultPrevented) {
+						navigation.navigate(route.name)
+					}
 				}
-			}
 
-			return (
-				<TabItemsWrapper key={route.name} onPress={onPress}>
-					{route.name === 'Bet' ? (
-						<BetIconContainer style={style.boxShadow}>
-							<BetIcon source={addBetIcon} />
-						</BetIconContainer>
-					) : (
-						<TabBarItem name={route.name} icon={icon} focused={isFocused} />
-					)}
-				</TabItemsWrapper>
-			)
-		})}
-	</TabBarContainer>
+				return (
+					<TabItemsWrapper key={route.name} onPress={onPress}>
+						{route.name === 'Bet' ? (
+							<BetIconContainer style={style.boxShadow}>
+								<BetIcon source={addBetIcon} />
+							</BetIconContainer>
+						) : (
+							<TabBarItem name={route.name} icon={icon} focused={isFocused} />
+						)}
+					</TabItemsWrapper>
+				)
+			})}
+		</TabBarContainer>
+	</TabBarWrapper>
 )
 
 export default TabBar
--- src/components/TabBar/styles.ts
+++ src/components/TabBar/styles.ts
@@ -2,11 +2,16 @@ import styled from 'styled-components/native'
 import colors from '../../assets/colors'
 import getDimensions from '../../utils/getDimensions'
 
+export const TabBarWrapper = styled.View`
+	background-color: ${colors.BG_LIGHT};
+`
+
 export const TabBarContainer = styled.View`
 	flex-direction: row;
 	height: ${`${getDimensions(4).rem}`};
 	background-color: white;
-	border-top-right-radius: 10px;
+	border-top-right-radius: ${getDimensions(1.5).rem};
+	border-top-left-radius: ${getDimensions(1.5).rem};
 `
 
 export const TabItemsWrapper = styled.TouchableOpacity`
@@ -15,8 +20,8 @@ export const TabItemsWrapper = styled.TouchableOpacity`
 `
 
 export const BetIconContainer = styled.View`
-	width: ${getDimensions(6).rem};
-	height: ${getDimensions(6).rem};
+	width: ${getDimensions(5).rem};
+	height: ${getDimensions(5).rem};
 	border-width: 5px;
 	border-color: white;
 
@@ -24,10 +29,10 @@ export const BetIconContainer = styled.View`
 	border-radius: ${`${getDimensions().height}px`};
 	justify-content: center;
 	align-items: center;
-	transform: translateY(-40px);
+	transform: translateY(-30px);
 `
 
 export const BetIcon = styled.Image`
-	width: ${getDimensions(3.5).rem};
-	height: ${getDimensions(3.5).rem};
+	width: ${getDimensions(2.5).rem};
+	height: ${getDimensions(2.5).rem};
 `
