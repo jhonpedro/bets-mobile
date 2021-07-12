38a2bf375255c1ab8b7afb26aef448ec32dcf13b
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-07-12 15:41:19 -0300

    feat: connect to API

--- src/components/Cart/Component/index.tsx
+++ src/components/Cart/Component/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
-import { Ionicons, MaterialCommunityIcons, Feather } from '@expo/vector-icons'
+import { Alert, TouchableOpacity } from 'react-native'
+import {
+	Ionicons,
+	MaterialCommunityIcons,
+	Feather,
+	AntDesign,
+} from '@expo/vector-icons'
 
 import {
+	actionClearCart,
 	actionHideCart,
 	actionRemoveFromCart,
 } from '../../../store/reducers/cart/actions'
@@ -11,13 +17,22 @@ import CartAnimation from '../Animation'
 import {
 	CartContainer,
 	CartContent,
+	CartFooter,
 	CartTitle,
+	CartTotal,
+	CartTotalContainer,
 	CloseCartIconContainer,
 	GameCartContainer,
+	GamesScrollView,
+	SaveButtonContainer,
+	TextStrong,
 } from './styles'
 import colors from '../../../assets/colors'
 import cartSelector from '../../../store/selectors/cartSelector'
 import Game from '../../Game/Component'
+import TextWithSVG from '../../TextWithSVG'
+import getDimensions from '../../../utils/getDimensions'
+import adonis from '../../../services/adonis'
 
 const Cart = () => {
 	const dispatch = useAppDispatch()
@@ -27,6 +42,30 @@ const Cart = () => {
 
 	const handleRemoveFromCart = (cartItemIndex: number) => {
 		dispatch(actionRemoveFromCart(cartItemIndex))
+	}
+
+	const handleSubmitNewPurchase = () => {
+		if (items.length === 0) {
+			return
+		}
+		try {
+			adonis.post('/purchases', {
+				bets: items.map((item) => ({
+					type: item.type,
+					game_id: item.game_id,
+					numbers: item.numbers,
+				})),
+			})
+
+			dispatch(actionClearCart())
+
+			Alert.alert(
+				'Success',
+				'Your bets were registered, you can see them in the home page now!'
+			)
+		} catch {
+			Alert.alert('Error', 'There were an error in your bets, try again later!')
+		}
 	}
 
 	return (
@@ -46,30 +85,56 @@ const Cart = () => {
 						/>
 						Cart
 					</CartTitle>
-					{items.map((item, index) => (
-						<GameCartContainer key={item.id}>
-							<Game
-								id={item.id}
-								color={item.color}
-								price={item.price}
-								created_at={item.created_at}
-								type={item.type}
-								numbers={item.numbers}
-								removable={{
-									index,
-									icon: (
-										<Feather
-											name="trash-2"
-											size={14}
-											color={colors.FONT_DARK}
-										/>
-									),
-									onPress: handleRemoveFromCart,
-								}}
-							/>
-						</GameCartContainer>
-					))}
+					<GamesScrollView>
+						{items.length === 0 && <TextStrong>Cart is empty!</TextStrong>}
+						{items.map((item, index) => (
+							<GameCartContainer key={item.id}>
+								<Game
+									id={item.id}
+									color={item.color}
+									price={item.price}
+									created_at={item.created_at}
+									type={item.type}
+									numbers={item.numbers}
+									removable={{
+										index,
+										icon: (
+											<Feather
+												name="trash-2"
+												size={14}
+												color={colors.FONT_DARK}
+											/>
+										),
+										onPress: handleRemoveFromCart,
+									}}
+								/>
+							</GameCartContainer>
+						))}
+					</GamesScrollView>
 				</CartContent>
+				<CartFooter>
+					<CartTotalContainer>
+						<CartTotal>
+							<TextStrong>Cart </TextStrong>
+							Total:
+						</CartTotal>
+						<TextStrong>00.00</TextStrong>
+					</CartTotalContainer>
+					<SaveButtonContainer onPress={handleSubmitNewPurchase}>
+						<TextWithSVG
+							text="Save"
+							fontSize={getDimensions(2).rem}
+							textColor={colors.TGL_GREEN}
+							svg={
+								<AntDesign
+									name="arrowright"
+									size={24}
+									color={colors.TGL_GREEN}
+								/>
+							}
+						/>
+					</SaveButtonContainer>
+				</CartFooter>
 			</CartContainer>
 		</CartAnimation>
 	)
--- src/components/Cart/Component/styles.ts
+++ src/components/Cart/Component/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native'
+import colors from '../../../assets/colors'
 import Text from '../../../assets/styles/Text'
 import getDimensions from '../../../utils/getDimensions'
 
@@ -9,7 +10,8 @@ export const CartContainer = styled.View`
 	margin-left: auto;
 	background-color: white;
 	padding-right: ${`${getDimensions(5).width}px`};
-	padding-left: ${`${getDimensions(5).width}px`};
+
+	justify-content: space-between;
 `
 
 export const CloseCartIconContainer = styled.View`
@@ -19,6 +21,7 @@ export const CloseCartIconContainer = styled.View`
 
 export const CartContent = styled.View`
 	justify-content: flex-start;
+	padding-left: ${`${getDimensions(5).width}px`};
 `
 
 export const CartTitle = styled(Text)`
@@ -27,7 +30,41 @@ export const CartTitle = styled(Text)`
 	font-weight: bold;
 `
 
+export const GamesScrollView = styled.ScrollView`
+	height: ${`${getDimensions(50).height}px`};
+	padding-top: 20px;
+`
+
 export const GameCartContainer = styled.View`
 	margin-top: ${`${getDimensions(2).height}px`};
 	margin-bottom: ${`${getDimensions(2).height}px`};
+`
+
+export const CartFooter = styled.View``
+
+export const TextStrong = styled(Text)`
+	font-weight: bold;
+	font-size: ${getDimensions(1.1).rem};
+	text-transform: uppercase;
+`
+
+export const CartTotalContainer = styled.View`
+	flex-direction: row;
+	justify-content: space-evenly;
+	margin-bottom: ${`${getDimensions(5).height}px`};
+`
+
+export const CartTotal = styled(TextStrong)`
+	font-weight: normal;
+`
+
+export const SaveButtonContainer = styled.TouchableOpacity`
+	background-color: ${colors.BORDER_INPUTS};
+	justify-content: center;
+	align-items: center;
+	padding-top: 20px;
+	padding-bottom: 40px;
+
+	border-top-right-radius: 10px;
+	border-top-left-radius: 10px;
 `
